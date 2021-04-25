9a0897ac9c96fc64c065c172e4f8130abd22ae50
h908714124 <kraftdurchblumen@gmx.de>
2021-04-25 11:53:51 +0200

    update dependencies

--- src/main/java/ich/bins/OperationCommand.java
+++ src/main/java/ich/bins/OperationCommand.java
@@ -3,7 +3,7 @@ package ich.bins;
 import net.jbock.Param;
 import net.jbock.SuperCommand;
 
-@SuperCommand("glacier-upload")
+@SuperCommand("glacier")
 abstract class OperationCommand {
 
     @Param(0)
